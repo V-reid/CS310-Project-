d9da8f1b37ae03c690075168ae83b436cd3c376c
Ryan Goh <ryangoh5c@gmail.com>
2025-05-13 12:25:47 +0300

    schedule page functionality added. with ability to add mockups to each day

--- fit_quest/lib/pages/schedulePage.dart
+++ fit_quest/lib/pages/schedulePage.dart
@@ -2,6 +2,48 @@ import 'package:fit_quest/common/common.dart';
 import 'package:fit_quest/common/layer.dart';
 import 'package:flutter/material.dart';
 
+import 'package:fit_quest/pages/mockup/mockupCard.dart';
+import 'package:fit_quest/pages/mockupSelectionPage.dart';
+
+const mockups = [
+  MockupCard(
+    name: "Saitama Training",
+    time: 90,
+    level: Difficulty.hard,
+    kcalBurn: 1500,
+    image: "assets/saitama.jpg",
+    mostPopular: true,
+    exercise: {
+      "Pushup": "100",
+      "Sits-up": "100",
+      "Squats": "100",
+      "Run": "10km",
+    },
+    rewards: {"Strength": "10", "Endurance": "15"},
+  ),
+  MockupCard(
+    name: "Rocky Training",
+    time: 60.5,
+    level: Difficulty.medium,
+    kcalBurn: 500,
+    image: "assets/rocky.jpg",
+    exercise: {"jump": "3x10", "run": "4km", "triceps extension": "3x10"},
+  ),
+  MockupCard(
+    name: "Yusuf Dikec Training",
+    time: 30,
+    level: Difficulty.easy,
+    kcalBurn: 145,
+    image: "assets/yusuf.webp",
+    exercise: {
+      "Pushup": "100",
+      "Sits-up": "100",
+      "Squats": "100",
+      "Run": "10km",
+    },
+  ),
+];
+
 class SchedulePage extends StatefulWidget {
   const SchedulePage({Key? key}) : super(key: key);
 
@@ -10,8 +52,19 @@ class SchedulePage extends StatefulWidget {
 }
 
 class _SchedulePageState extends State<SchedulePage> {
+  int selectedDayIndex = DateTime.now().weekday - 1;
+  final Map<int, List<MockupCard>> trainingSchedule = {
+    0: [mockups[0]], // Monday
+    2: [mockups[1]], // Wednesday
+    6: [mockups[2]], // Sunday
+  };
+
   @override
   Widget build(BuildContext context) {
+    final todayIndex = DateTime.now().weekday - 1;
+    // final weekday = todayIndex;
+    // final days = Common.days; // ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
+    // int selectedDayIndex = today.weekday;
     return pageLayer(
       context: context,
       pageName: "SCHEDULE",
@@ -21,19 +74,63 @@ class _SchedulePageState extends State<SchedulePage> {
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children:
-                  Common.days.map((x) => Common.dayButton(text: x)).toList(),
+              // children:
+              //     Common.days.map((x) => Common.dayButton(text: x)).toList(),
+              children: List.generate(Common.days.length, (index) {
+                final isToday = index == todayIndex;
+                final isSelected = index == selectedDayIndex;
+                return Common.dayButton(
+                  text: Common.days[index],
+                  isToday: isToday,
+                  isSelected: isSelected,
+                  onTap: () {
+                    setState(() {
+                      selectedDayIndex = index;
+                    });
+                  },
+                );
+              }),
             ),
             Column(
-              spacing: 20,
-              children: [
-                Text("START NOW!", style: TextStyle(fontSize: 20)),
-                ElevatedButton(
-                  onPressed: () => {},
-                  child: Icon(Icons.add, size: 30),
-                ),
-              ],
+              children: trainingSchedule[selectedDayIndex] ??
+                  [
+                    Text("Rest Day or Slack Day? \n START NOW!", style: TextStyle(fontSize: 20), textAlign: TextAlign.center),
+                        ElevatedButton(
+                          onPressed: () async {
+                            final selected = await Navigator.push<List<MockupCard>>(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => MockupSelectionPage(
+                                  availableMockups: mockups,
+                                ),
+                              ),
+                            );
+
+                            if (selected != null && selected.isNotEmpty) {
+                              setState(() {
+                                trainingSchedule.update(
+                                  selectedDayIndex,
+                                      (list) => [...list, ...selected],
+                                  ifAbsent: () => selected,
+                                );
+                              });
+                            }
+                          },
+                          child: Icon(Icons.add, size: 30),
+                        ),
+                  ],
             ),
+            // Column(
+            //   spacing: 20,
+            //   children: [
+            //     Text("START NOW!", style: TextStyle(fontSize: 20)),
+            //     ElevatedButton(
+            //       // TODO: show options, each day should be a storable
+            //       onPressed: () => {},
+            //       child: Icon(Icons.add, size: 30),
+            //     ),
+            //   ],
+            // ),
           ],
         ),
       ),
